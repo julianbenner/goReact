2d0b2d2cc25c07b0df46e0ee1e93fdf73ca8c61b
Julian Benner <julian.benner@hpe.com>
2016-02-22 09:59:28 +0100

    Fixes notifications for good, hopefully

--- client/src/reducers/game.ts
+++ client/src/reducers/game.ts
@@ -22,14 +22,15 @@ export default handleActions<GameState>({
     },
     [RECEIVE_GAME]: (state: GameState, action: Action): GameState => {
         const newGame = <ClientGame>action.payload.game;
-        if (state.game !== null) {
-            if (state.game.turn && newGame.color === Piece.White || !state.game.turn && newGame.color === Piece.Black) {
+        if (newGame !== null) {
+            if (newGame.turn && newGame.color === Piece.White || !newGame.turn && newGame.color === Piece.Black) {
                 if ('Notification' in window) {
                     let _Notification = window['Notification'];
                     if (_Notification.permission === 'granted') {
+                        const color = newGame.turn ? './white.png' : './black.png';
                         const options = {
                             body: 'It\'s your turn!',
-                            icon: './white.png'
+                            icon: color
                         };
                         const n = new _Notification('Go', options);
                         setTimeout(n.close.bind(n), 5000);
